020019ed6b376b90a193716c55e2d7f79aab5c92
amaliekea <amje0001@stud.kea.dk>
2024-04-23 12:48:48 +0200

    fjernet scanner, givet parametre med i main i stedet

--- src/Main.java
+++ src/Main.java
@@ -4,40 +4,22 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("velkommen til salget af tilletter til Amalie's event!");
-        Scanner scanner = new Scanner(System.in);
-
-
-        System.out.println("Dage til event, Studiebillet:");
-        int dageTilEvent = scanner.nextInt();
-        int studieId=12345;
-        if(dageTilEvent>10) {
-            System.out.println("indtast studie id: ");
-            studieId = scanner.nextInt();
-        }
-        System.out.println("BilletID til studiebillet: ");
-        int billetId = scanner.nextInt();
-        Billet studieBillet = new StudieBillet(billetId, dageTilEvent, studieId);
+        Billet studieBillet = new StudieBillet(1249202, 22, 988810);
         studieBillet.getPrice();
         System.out.println(studieBillet);
 
-
-        System.out.println("ID til dørsalg: ");
-        billetId = scanner.nextInt();
-        Billet dørBillet = new DørSalg(billetId);
+        Billet dørBillet = new DørSalg(29011);
         System.out.println(dørBillet);
 
-        System.out.println("Dage til event, forsalg:");
-        dageTilEvent = scanner.nextInt();
-        System.out.println("ID til forsalg: ");
-        billetId = scanner.nextInt();
-        Billet forsalgBillet = new ForSalg(billetId,dageTilEvent);
+        Billet forsalgBillet = new ForSalg(1919201,2);
         forsalgBillet.getPrice();
         System.out.println(forsalgBillet);
-        ///////
+
 
         SolgteBilletter solgtb = new SolgteBilletter();
-        solgtb.addBillet(studieBillet);
+        solgtb.addBillet(studieBillet); //tilføj til liste af solgte biletter
         solgtb.addBillet(dørBillet);
         solgtb.addBillet(forsalgBillet);
 
